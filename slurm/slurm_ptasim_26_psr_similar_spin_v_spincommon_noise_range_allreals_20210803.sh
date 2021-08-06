764aae2fb89098579ed78bd554bc7a6a5fac878a
Andrew Zic <andrew.john.zic@gmail.com>
2021-08-06 16:07:47 +1000

    made slurm batch script for noise range

--- slurm/slurm_ptasim_26_psr_similar_spin_v_spincommon_noise_range_allreals_20210803.sh
+++ slurm/slurm_ptasim_26_psr_similar_spin_v_spincommon_noise_range_allreals_20210803.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=gwb_crn_sims_mc_spin_v_spincommon_noise_range_allreal
 #SBATCH --output=/flush5/zic006/gwb_crn_sims/slurm_logs/gwb_crn_sims_mc_spin_v_spincommon_noise_range_20210803_%A_%a.log
-#SBATCH --ntasks=10
+#SBATCH --ntasks=4
 #SBATCH --time=0-12:00:00
 #SBATCH --mem-per-cpu=32G
 #SBATCH --tmp=8G
-#SBATCH --array=0-49
+#SBATCH --array=0-1210
 
 # pyv="$(python -c 'import sys; print(sys.version_info[0])')"
 # if [ "$pyv" == 2 ]
@@ -23,9 +23,7 @@ singularity exec /home/zic006/psr_gwb.sif echo $TEMPO2_CLOCK_DIR
 
 echo $SLURM_ARRAY_TASK_ID
 
-real=$(($SLURM_ARRAY_TASK_ID / 5 ))
-noise_n=$(($SLURM_ARRAY_TASK_ID % 5 + 1))
-echo "processing realisation ${real} for noise level index ${noise_n}"
+paramfiles=(/flush5/zic006/gwb_crn_sims/params/all_mc_array_spin_v_spincommon/params_all_mc_array_spin_v_spincommon_[0-9]*.dat)
+echo "processing paramfile ${paramfiles[$SLURM_ARRAY_TASK_ID]}"
 
-echo "/flush5/zic006/pptadr2_gwb_crn_sims/ptasim_26psr_similar/params/params_all_mc_array_spin_v_spincommon_N"${noise_n}"_20210524_r${real}.dat"
-singularity exec /home/zic006/psr_gwb.sif python3 /flush5/zic006/pptadr2_gwb_crn_sims/ptasim_26psr_similar/run_enterprise_simple.py --prfile "/flush5/zic006/pptadr2_gwb_crn_sims/ptasim_26psr_similar/params/params_all_mc_array_spin_v_spincommon_N"${noise_n}"_20210524_r${real}.dat"
+singularity exec /home/zic006/psr_gwb.sif python3 /flush5/zic006/gwb_crn_sims/run_enterprise_simple.py --prfile ${paramfiles[$SLURM_ARRAY_TASK_ID]}
