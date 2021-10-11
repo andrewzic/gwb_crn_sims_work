f0732ba009560fc43fbc951d4e7f94ea8ae153d7
andrewzic <andrew.john.zic@gmail.com>
2021-10-11 13:52:28 +1100

    new copy_edit_paramfile for 100 realisation fixed gamma pe runs

---
+++ params/all_pe_array_spincommonfixgamma/copy_edit_paramfile_noise_range_realisations.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+for N in `ls ../../psr_noise_vals/100r/[0-9]*.dat`; 
+do
+    delta=`basename $N .dat`
+    echo $delta ;
+    for real in `seq 0 9`; 
+    do 
+	echo $r ;
+	newpf=params_all_pe_array_spincommonfixgamma_${delta}_20210803_r${real}.dat
+	cp params_all_pe_array_spincommonfixedgamma_DP0_DALPHA_20210803_rN.dat $newpf;
+	sed -i 's|DP0_DALPHA|'${delta}'|g' $newpf;
+	sed -i 's|N|'${real}'|g' $newpf;
+	#sed -i 's|zic006/ssb/ptasim/|zic006/pptadr2_gwb_crn_sims/|g' $newpf
+    done
+done
+
