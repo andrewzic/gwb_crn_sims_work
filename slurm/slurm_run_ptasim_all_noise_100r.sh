d764f417d7d70e103783cd72364b22b13bfde08c
Andrew <andrew.john.zic@gmail.com>
2021-10-12 10:25:28 +1100

    slurm ptasim runscript set to 1 hour runtime

--- slurm/slurm_run_ptasim_all_noise_100r.sh
+++ slurm/slurm_run_ptasim_all_noise_100r.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=/flush5/zic006/gwb_crn_sims/slurm_logs/run_ptasim_all_noise_100r_%A_%a.log
 #SBATCH --ntasks-per-node=10
 #SBATCH --nodes=1
-#SBATCH --time=0-10:00:00
+#SBATCH --time=0-01:00:00
 #SBATCH --mem=16G
 #SBATCH --tmp=8G
 #SBATCH --array=0-121
